221ebf4b5a2fbe7c8053ba8e9a58e2e96bd03eca
Daniel Ziegler <info@nook24.eu>
2019-02-22 08:55:48 +0100

    Add orange color

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # [nixie-clock](http://test.bentswanson.com/nixie)
 
 > A JavaScript clock imitating [Nixie tubes](https://en.wikipedia.org/wiki/Nixie_tube).
+
+## Example Yellow (default)
+![Yellow Nixie clock](screenshots/yellow.png "Yellow Nixie clock")
+
+## Example Orange
+Add the class `clock-orange`.
+![Orange Nixie clock](screenshots/orange.png "Orange Nixie clock")
--- nixie.js
+++ nixie.js
@@ -1,9 +1,9 @@
-const hours = document.querySelector('.hours')
-const mins = document.querySelector('.mins')
-const secs = document.querySelector('.secs')
+const hours = document.querySelector('.hours');
+const mins = document.querySelector('.mins');
+const secs = document.querySelector('.secs');
 
 function getTime() {
-    const now = new Date()
+    const now = new Date();
     return {
         hours: now.getHours() <= 9 ? `0${now.getHours()}` : `${now.getHours()}`,
         mins: now.getMinutes() <= 9 ? `0${now.getMinutes()}` : `${now.getMinutes()}`,
@@ -12,21 +12,21 @@ function getTime() {
 }
 
 function setDigits(section, digit) {
-    const tens = [...section.children[0].children]
-    const ones = [...section.children[1].children]
-    tens.forEach(number => number.classList.remove('active'))
-    tens[digit[0]].classList.add('active')
-    ones.forEach(number => number.classList.remove('active'))
-    ones[digit[1]].classList.add('active')
+    const tens = [...section.children[0].children];
+    const ones = [...section.children[1].children];
+    tens.forEach(number => number.classList.remove('active'));
+    tens[digit[0]].classList.add('active');
+    ones.forEach(number => number.classList.remove('active'));
+    ones[digit[1]].classList.add('active');
 }
 
 function tick() {
-    const time = getTime()
-    setDigits(hours, time.hours)
-    setDigits(mins, time.mins)
-    setDigits(secs, time.secs)
+    const time = getTime();
+    setDigits(hours, time.hours);
+    setDigits(mins, time.mins);
+    setDigits(secs, time.secs);
 }
 
-tick()
+tick();
 
-setInterval(tick, 1000)
+setInterval(tick, 1000);
